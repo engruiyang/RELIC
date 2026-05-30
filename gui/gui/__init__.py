a8b40c77a994b0d1623561807198fdd606258fe6
engruiyang <118709609+engruiyang@users.noreply.github.com>
2026-05-30 20:16:26 +0800

    Delete gui/gui directory

--- gui/gui/__init__.py
+++
@@ -1,6 +0,0 @@
-"""GUI adapter layer for RELIC minimal Qt probe."""
-
-from .gui_facade import GuiFacade
-from .gui_protocol import GuiPacket, GUI_PACKET_TYPES
-
-__all__ = ["GuiFacade", "GuiPacket", "GUI_PACKET_TYPES"]
